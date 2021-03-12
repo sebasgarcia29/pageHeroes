c4a3647a07e779fe8c9c81c81fd81ed83ab46ba9
sebasgarcia29 <s.garcia@iimoney.co>
2021-03-11 23:25:04 -0500

    update with links secures

--- src/components/ui/NavBar.js
+++ src/components/ui/NavBar.js
@@ -1,58 +1,71 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Link, NavLink, useHistory } from 'react-router-dom';
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 export const Navbar = () => {
+  const { user, dispatch } = useContext(AuthContext);
+  const history = useHistory();
+
+  const handleLogout = () => {
+    history.replace('/login');
+    dispatch({
+      type: types.logout,
+    });
+  }
+
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+    <nav className='navbar navbar-expand-sm navbar-dark bg-dark'>
 
       <Link
-        className="navbar-brand"
-        to="/"
+        className='navbar-brand'
+        to='/'
       >
         Asociaciones
       </Link>
 
-      <div className="navbar-collapse">
-        <div className="navbar-nav">
+      <div className='navbar-collapse'>
+        <div className='navbar-nav'>
 
           <NavLink
-            activeClassName="active"
-            className="nav-item nav-link"
+            activeClassName='active'
+            className='nav-item nav-link'
             exact
-            to="/marvel"
+            to='/marvel'
           >
             Marvel
           </NavLink>
 
           <NavLink
-            activeClassName="active"
-            className="nav-item nav-link"
+            activeClassName='active'
+            className='nav-item nav-link'
             exact
-            to="/dc"
+            to='/dc'
           >
             DC
           </NavLink>
           <NavLink
-            activeClassName="active"
-            className="nav-item nav-link"
+            activeClassName='active'
+            className='nav-item nav-link'
             exact
-            to="/search"
+            to='/search'
           >
             Search
           </NavLink>
         </div>
       </div>
 
-      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
-        <ul className="navbar-nav ml-auto">
-          <NavLink
-            activeClassName="active"
-            className="nav-item nav-link"
-            exact
-            to="/login"
+      <div className='navbar-collapse collapse w-100 order-3 dual-collapse2'>
+        <ul className='navbar-nav ml-auto'>
+          <span className="nav-item nav-link text-info">
+            {user.name}
+          </span>
+          <button
+            className='nav-item nav-link btn'
+            onClick={handleLogout}
           >
             Logout
-          </NavLink>
+          </button>
         </ul>
       </div>
     </nav>
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
 } from "react-router-dom";
+import { PublicRoute } from './PublicRoute';
+import { PrivateRoute } from './PrivateRoute';
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashBoardRoutes } from './DashBoardRoutes';
+import { AuthContext } from '../auth/AuthContext';
 
 export const AppRouter = () => {
+
+  const { user } = useContext(AuthContext);
+
   return (
     <Router>
       <div>
         <Switch>
-          <Route exact path="/login" component={LoginScreen} />
-          <Route path="/" component={DashBoardRoutes} />
+          <PublicRoute
+            exact
+            path='/login'
+            component={LoginScreen}
+            isAuthenticated={user.logged}
+          />
+          <PrivateRoute
+            path='/'
+            component={DashBoardRoutes}
+            isAuthenticated={user.logged}
+          />
         </Switch>
       </div>
     </Router>
